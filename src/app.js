c3f173839b2fdc52494ef3dc8b0e4d54bf8fabae
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-10-31 08:41:45 -0300

    feat:finalizacao da entidade livro com todas as operacoes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectaDataBase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
 const conexao = await connectaDataBase();
 conexao.on("error", (erro) => {
@@ -11,47 +12,7 @@ conexao.once("open", () => {
 });
 
 const app = express();
-app.use(express.json());
-
-const livros = [
-  {
-    id: 1,
-    titulo: "o senhor dos aneis",
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit",
-  },
-];
-
-const buscaLivros = (id) => {
-  return livros.findIndex((livro) => livro.id === Number(id));
-};
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de nodeJS");
-});
-
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const { id } = req.params;
-  const index = buscaLivros(id);
-  res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send("Livro cadastrado com sucesso!");
-});
-
-app.put("/livros/:id", (req, res) => {
-  const { id } = req.params;
-  const index = buscaLivros(id);
-  livros[index].titulo = req.body.titulo;
-  res.status(200).json(livros);
-});
+routes(app)
 
 app.delete("/livros/:id", (req, res) => {
   const { id } = req.params;
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,8 @@
 import mongoose, { mongo } from "mongoose";
 
 async function connectaDataBase(){
-    mongoose.connect(
-        "mongodb+srv://admin:admin123@cluster0.cfcbzfp.mongodb.net/livraria?retryWrites=true&w=majority"
-      );
-      return mongoose.connection
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
+    return mongoose.connection
 }
 
 export default connectaDataBase
